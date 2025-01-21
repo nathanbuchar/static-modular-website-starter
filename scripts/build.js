4f0baf7e7a601fecaaed93f357c6f98e69c25063
Nate M <hello@n8.engineer>
2025-01-21 10:05:25 -0500

    Clean up build system

--- scripts/build.js
+++ scripts/build.js
@@ -16,14 +16,18 @@ import path from 'path';
 /**
  * @function RenderFn
  * @param {string} template
- * @param {Object} context
+ * @param {Context} ctx
  * @returns Promise<string>
+ */
+
+/**
+ * @typedef {Object.<string, any>} Context
  */
 
 /**
  * @function Plugin
  * @param {Config} config
- * @param {Object} ctx
+ * @param {Context} ctx
  * @returns {Promise<void>}
  */
 
@@ -37,163 +41,179 @@ import path from 'path';
 
 /**
  * @function TargetFn
- * @param {Object} ctx
+ * @param {Context} ctx
  * @returns {(Target | TargetFn)[]}
  */
 
-/**
- * Writes a file.
- *
- * @async
- * @param {string} pathToFile
- * @param {string} data
- * @returns {Promise<void>}
- */
-async function writeFile(pathToFile, data) {
-  await new Promise((resolve, reject) => {
-    const dirname = path.dirname(pathToFile);
+class Builder {
 
-    fs.mkdir(dirname, { recursive: true }, (err) => {
-      if (err) {
-        reject(err);
+  /** @type {Context} */
+  static #ctx = {};
+
+  /** @type {Config} */
+  static #config = {};
+
+  /**
+   * Writes a file.
+   *
+   * @async
+   * @param {string} filePath
+   * @param {string} data
+   * @returns {Promise<void>}
+   */
+  static async writeFile(filePath, data) {
+    await new Promise((resolve, reject) => {
+      const normalizedFilePath = path.normalize(filePath);
+      const dirname = path.dirname(normalizedFilePath);
+
+      fs.mkdir(dirname, { recursive: true }, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          fs.writeFile(normalizedFilePath, data, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              console.log(`Wrote file "${normalizedFilePath}"`);
+              resolve();
+            }
+          });
+        }
+      });
+    });
+  }
+
+  /**
+   * Renders a target.
+   *
+   * @async
+   * @param {Target} target
+   * @param {Context} ownCtx
+   * @returns {Promise<void>}
+   */
+  static async renderTarget(target, ownCtx) {
+    const config = Builder.#config;
+    
+    const normalizedTemplatePath = path.normalize(target.template);
+    const res = await config.engine.render(normalizedTemplatePath, ownCtx);
+
+    await Builder.writeFile(target.dest, res);
+  }
+
+  /**
+   * Builds a target.
+   *
+   * @async
+   * @param {Target} target
+   * @returns {Promise<void>}
+   */
+  static async buildTarget(target) {
+    const ctx = Builder.#ctx;
+    const ownCtx = {};
+
+    // Apply included context.
+    if (target.include) {
+      if (target.include === '*') {
+        // Handle wildcard case.
+        Object.assign(ownCtx, ctx);
       } else {
-        fs.writeFile(pathToFile, data, (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            console.log(`Wrote file "${pathToFile}"`);
-            resolve();
-          }
+        target.include.forEach((key) => {
+          ownCtx[key] = ctx[key];
         });
       }
-    });
-  });
-}
+    }
 
-/**
- * Renders a target.
- *
- * @async
- * @param {Config} config
- * @param {Target} target
- * @param {Object} ctx
- * @returns {Promise<void>}
- */
-async function renderTarget(config, target, ctx) {
-  const template = path.normalize(target.template);
-  const dest = path.normalize(target.dest);
-  
-  const res = await config.engine.render(template, ctx);
+    // Apply extra context.
+    if (target.extraContext) {
+      Object.assign(ownCtx, target.extraContext);
+    }
 
-  await writeFile(dest, res);
-}
+    await Builder.renderTarget(target, ownCtx);
+  }
 
-/**
- * Builds a target.
- *
- * @async
- * @param {Config} config
- * @param {Object} ctx
- * @param {Target} target
- * @returns {Promise<void>}
- */
-async function buildTarget(config, ctx, target) {
-  const ownCtx = {};
+  /**
+   * Builds all targets recursively.
+   *
+   * @async
+   * @param {(Target | TargetFn)[]} targets
+   * @returns {Promise<void>}
+   */
+  static async buildTargets(targets) {
+    const ctx = Builder.#ctx;
+    const config = Builder.#config;
 
-  // Apply included context.
-  if (target.include) {
-    if (target.include === '*') {
-      // Handle wildcard case.
-      Object.assign(ownCtx, ctx);
-    } else {
-      target.include.forEach((key) => {
-        ownCtx[key] = ctx[key];
-      });
+    for (const target of targets ?? config.targets) {
+      let currTarget = target;
+
+      if (Array.isArray(currTarget)) {
+        return Builder.buildTargets(currTarget);
+      } else {
+        const ownCtx = {};
+
+        // Derive target if target is a function.
+        if (typeof currTarget === 'function') {
+          currTarget = await target(ctx);
+
+          if (Array.isArray(currTarget)) {
+            await Builder.buildTargets(currTarget);
+            return;
+          }
+        }
+
+        await Builder.buildTarget(currTarget);
+      }
     }
   }
 
-  // Apply extra context.
-  if (target.extraContext) {
-    Object.assign(ownCtx, target.extraContext);
-  }
+  /**
+   * Runs all plugins synchronously.
+   * 
+   * @returns {Promise<void>}
+   */
+  static async runPlugins() {
+    const ctx = Builder.#ctx;
+    const config = Builder.#config;
 
-  await renderTarget(config, target, ownCtx);
-}
-
-/**
- * Builds all targets recursively.
- *
- * @async
- * @param {Config} config
- * @param {Object} ctx
- * @param {(Target | TargetFn)[]} targets
- * @returns {Promise<void>}
- */
-async function buildTargets(config, ctx, targets) {
-  for (const target of targets) {
-    if (typeof target === 'function') {
-      const newTarget = await target(ctx);
-      const newTargetArr = Array.isArray(newTarget) ? newTarget : [newTarget];
-
-      await buildTargets(config, ctx, newTargetArr);
-    } else if (Array.isArray(target)) {
-      return buildTargets(config, ctx, target);
-    } else {
-      await buildTarget(config, ctx, target);
+    for (const plugin of config.plugins) {
+      await plugin(config, ctx);
     }
   }
-}
 
-/**
- * Runs all plugins synchronously.
- * 
- * @param {Config} config
- * @param {Object} ctx
- * @returns {Promise<void>}
- */
-async function runPlugins(config, ctx) {
-  for (const plugin of config.plugins) {
-    await plugin(config, ctx);
+  /**
+   * Reads the config file from the directory in which npm
+   * was invoked.
+   * 
+   * @async
+   * @returns {Promise<void>}
+   */
+  static async getConfig() {
+    const npmDir = process.cwd();
+    const pathToConfig = path.resolve(npmDir, 'config.js');
+
+    try {
+      const mod = await import(pathToConfig);
+
+      Builder.#config = {
+        plugins: [],
+        targets: [],
+        ...mod.default,
+      };
+    } catch (err) {
+      throw new Error(`Config file could not be loaded: ${err}`);
+    }
+  }
+
+  /**
+   * Builds the static site.
+   *
+   * @async
+   * @returns {Promise<void>}
+   */
+  static async init() {
+    await Builder.getConfig();
+    await Builder.runPlugins();
+    await Builder.buildTargets();
   }
 }
 
-/**
- * Reads the config file from the directory in which npm
- * was invoked.
- * 
- * @async
- * @returns {Promise<Config>}
- */
-async function getConfig() {
-  const npmDir = process.cwd();
-  const pathToConfig = path.resolve(npmDir, 'config.js');
-
-  try {
-    const mod = await import(pathToConfig);
-
-    return {
-      plugins: [],
-      targets: [],
-      ...mod.default,
-    };
-  } catch (err) {
-    throw new Error(`Config file could not be loaded: ${err}`);
-  }
-}
-
-/**
- * Builds the static site.
- *
- * @async
- * @returns {Promise<void>}
- */
-async function build() {
-  const ctx = {};
-
-  const config = await getConfig();
-
-  await runPlugins(config, ctx);
-  await buildTargets(config, ctx, config.targets);
-}
-
-build();
+Builder.init();
